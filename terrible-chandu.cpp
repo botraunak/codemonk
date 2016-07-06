7656f6811d9d1bac441e80a48b317db8d40760fd
Raunak Sett <sett.raunak@gmail.com>
2016-07-06 16:13:31 +0530

    string problem 1 done

---
+++ terrible-chandu.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--){
+		string s,r;
+		cin>>s;
+		int len =0;
+		for(;s[len] != '\0';len++);
+
+		int i = 0;
+		for(;i<len;i++)
+			r[i] = s[len-1-i];
+
+		r[i] = '\0';
+
+		for (i = 0; i < len; ++i)
+			cout<<r[i];
+		cout<<endl;
+
+				
+	}
+}
